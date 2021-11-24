69c524a0eac19b4e44988eb98bf1d1c3e44d7ea0
vovanbart <vovanbart@icloud.com>
2021-11-24 20:47:39 +0300

    9

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -8,6 +8,7 @@ from .permissions import AuthorPermission
 from rest_framework.filters import SearchFilter
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.pagination import LimitOffsetPagination
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -16,6 +17,7 @@ class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (AuthorPermission,)
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['group', ]
+    pagination_class = LimitOffsetPagination 
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
